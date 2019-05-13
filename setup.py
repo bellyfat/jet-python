fc5d6e0790587dcf7b83670f4aa6f24e8c8d77e0
Tarun Bhardwaj <tarun.bhardwaj@fulfil.io>
2019-05-13 18:14:34 +0530

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='jet-python',
-    version='0.1',
+    version='0.2',
     description='Python Jet.com API Client',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Fulfil.IO Inc.',
